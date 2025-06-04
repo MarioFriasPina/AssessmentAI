db9a955f5f947e581fd9317db828e859a5c79e84
Alex F <alejandro.fernandez@roue.com.mx>
2025-06-04 09:36:41 -0600

    auth created

--- backend/main.py
+++ backend/main.py
@@ -4,23 +4,155 @@ from contextlib import asynccontextmanager
 import logging
 import json
 import aiohttp
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 import gymnasium as gym
+from pylibsrtp import Session
 import uvicorn
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect, Request
+from fastapi import Depends, FastAPI, WebSocket, WebSocketDisconnect, Request, HTTPException
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from aiortc import RTCPeerConnection, RTCSessionDescription, VideoStreamTrack
 from av import VideoFrame
+from sqlalchemy import create_engine, MetaData, Column, Integer, String, Select
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from pydantic import BaseModel
+import bcrypt
+import jwt
+import base64
+from datetime import datetime, timedelta
 
+import os
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+
+
+# region Conf
+
+DATABASE_URL = "sqlite+aiosqlite:///./test.db"  # Change this to your database URL
+
+# Create the SQLAlchemy engine
+engine_base = create_async_engine(DATABASE_URL, echo=True)
+engine = sessionmaker(
+    bind=engine_base,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
+# Create a configured "Session" class
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Create a base class for declarative models
+Base = declarative_base()
+
+# Create a metadata instance
+metadata = MetaData()
+
+# end region
+
+# region Crypt
+
+SECRET_KEY = os.getenv('SECRET', '123')
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+def hash_password(password: str) -> str:
+    # Hash the password using bcrypt
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode(encoding='utf-8'), salt)
+    return base64.b64encode(hashed).decode('utf-8')  # Encode as base64
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    # Decode the base64 encoded hashed password
+    decoded_hashed = base64.b64decode(hashed_password.encode('utf-8'))
+    return bcrypt.checkpw(plain_password.encode('utf-8'), decoded_hashed)
+
+
+def create_access_token(data: dict, expires_delta: timedelta = None) -> str:
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now() + expires_delta
+    else:
+        expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        # You can add additional checks here, e.g., checking the user ID or roles
+        return payload  # Return the payload if the token is valid
+    except jwt.PyJWTError as e:
+
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+# end region
+# region Databases
+
+
+async def get_db() -> AsyncSession:
+    async with engine() as session:
+        yield session
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    name_last = Column(String)
+    email = Column(String, unique=True, index=True)
+    password = Column(String, nullable=False)
+
+    runs = Column(Integer, default=0)
+    wins = Column(Integer, default=0)
+
+    record = Column(Integer, default=0)
+
+# end region
+
+# region pydantic def
+
+
+class NewUser(BaseModel):
+    name: str
+    email: str
+    name_last: str
+    password: str
+
+
+class LoginData(BaseModel):
+    username: str
+    password: str
+
+
+class UserData(BaseModel):
+    win: int
+    loss: int
+    total: int
+    record: float
+
+
+# end region
+
+
+# region Fastapi
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Load the ML model
     logger.info("Shutting down PeerConnections")
     await asyncio.gather(*[pc.close() for pc in pcs])
+    await init_db()
     pcs.clear()
     yield
-
 
 app = FastAPI(lifespan=lifespan)
 
@@ -40,6 +172,7 @@ latest_actions = {}
 
 MODEL_URL = "http://192.24.0.9:443/predict"
 
+
 # CORS
 app.add_middleware(
     CORSMiddleware,
@@ -49,6 +182,67 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
+async def init_db():
+    async with engine_base.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+# end region
+# region login
+
+
+@app.post('/token')
+async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: AsyncSession = Depends(get_db)) -> str:
+    query = Select(User).where(User.email == form_data.username)
+    result = await db.execute(query)
+
+    res = result.first()
+
+    if res is None:
+        raise HTTPException(403, 'Invalid username or password')
+
+    user: User = res[0]
+
+    if verify_password(form_data.password, user.password):
+
+        return create_access_token(
+            {
+                'user': user.name,
+                'email': user.email,
+            }
+        )
+
+    raise HTTPException(403, 'Invalid username or password')
+
+
+@app.post('/create')
+async def create_user(data: NewUser, db: AsyncSession = Depends(get_db)) -> str:
+    newUser = User(
+        name=data.name,
+        name_last=data.name_last,
+        email=data.email,
+        password=hash_password(data.password),
+    )
+    db.add(newUser)
+    await db.commit()
+
+    return "success"
+
+
+@app.get('/data')
+async def get_data(current_user: dict = Depends(get_current_user)):
+
+    return UserData(
+        win=0,
+        loss=0,
+        total=0,
+        record=0,
+    )
+
+
+# end region
+# region Video endpoints
 @app.websocket("/ws")
 async def websocket_handler(websocket: WebSocket):
     await websocket.accept()
@@ -74,6 +268,7 @@ async def websocket_handler(websocket: WebSocket):
     finally:
         latest_actions.pop(session_id, None)
 
+
 class UserVideoTrack(VideoStreamTrack):
     def __init__(self, env, session_id):
         super().__init__()
@@ -92,6 +287,7 @@ class UserVideoTrack(VideoStreamTrack):
         video_frame.pts = pts
         video_frame.time_base = time_base
         return video_frame
+
 
 class RLVideoTrack(VideoStreamTrack):
     def __init__(self, env, model_url, skip=2):
@@ -149,6 +345,7 @@ class RLVideoTrack(VideoStreamTrack):
         video_frame.time_base = time_base
         return video_frame
 
+
 @app.post("/offer")
 async def offer(request: Request):
     params = await request.json()
@@ -166,9 +363,11 @@ async def offer(request: Request):
 
     await pc.setRemoteDescription(_offer)
 
-    env_user = gym.make("CarRacing-v3", render_mode="rgb_array", continuous=False)
+    env_user = gym.make(
+        "CarRacing-v3", render_mode="rgb_array", continuous=False)
     env_user.reset()
-    env_rl = gym.make("CarRacing-v3", render_mode="rgb_array", continuous=False)
+    env_rl = gym.make(
+        "CarRacing-v3", render_mode="rgb_array", continuous=False)
     env_rl.reset()
 
     video_track_user = UserVideoTrack(env_user, session_id)
@@ -186,4 +385,4 @@ async def offer(request: Request):
     }
 
 
-
+# end region
