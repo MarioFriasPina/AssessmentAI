395e664c6ba850a0e6380bf82983882a8af689a5
Alank99 <alan_hdz129@hotmail.com>
2025-06-05 03:14:02 -0600

    Generate cloud init for the database and frontend

--- frontend2/src/assets/js/pages/ScoreBoard.js
+++ frontend2/src/assets/js/pages/ScoreBoard.js
@@ -21,3 +21,30 @@ document.addEventListener("DOMContentLoaded", () => {
     tableBody.appendChild(row);
   });
 });
+
+// Function to display metrics in the modal
+
+document.addEventListener("DOMContentLoaded", () => {
+  const stats = {
+    bestTime: "12 minutes",
+    avgTime: "5 minutes",
+    wins: "17 wins",
+    losses: "8 losses"
+  };
+
+  //check if the data is null or undefined
+  //if null or undefined, we will display 0
+    if (!stats.bestTime) stats.bestTime = "0 minutes";
+    if (!stats.avgTime) stats.avgTime = "0 minutes";
+    if (!stats.wins) stats.wins = "0 wins";
+    if (!stats.losses) stats.losses = "0 losses";
+
+
+  // Insertamos los valores en el HTML
+  document.getElementById("bestTime").textContent = stats.bestTime;
+  document.getElementById("avgTime").textContent = stats.avgTime;
+  document.getElementById("wins").textContent = stats.wins;
+  document.getElementById("losses").textContent = stats.losses;
+  
+});
+
--- frontend2/src/assets/js/pages/signup.js
+++ frontend2/src/assets/js/pages/signup.js
@@ -2,25 +2,32 @@
 // 1. Configuration
 // --------------
 const backendHost = "172.24.0.83:443"; // your backend address (with port)
-const REST_URL = `https://${backendHost}/token`;
+const REST_URL = `https://${backendHost}/create`;
 
 // Wait for the entire DOM to be loaded before running any code
 document.addEventListener("DOMContentLoaded", () => {
     // -------------- 
     // get references to the login form the button sign in
     // --------------
-    const loginButton = document.getElementById("loginButton");
+    const signUp = document.getElementById("signUpButton");
 
     // add a click event listener to the login button
-    loginButton.addEventListener("click", async function () {
+    signUp.addEventListener("click", async function () {
         //Get the info the values from the email and password fields
+        const name = document.getElementById("name").value;
+        const last_name = document.getElementById("last_name").value;
         const email = document.getElementById("email").value;
         const password = document.getElementById("password").value;
 
+
         // Prepare form data as URL
         const formData = new URLSearchParams();
-        formData.append("username", email);
+        formData.append("name", name);
+        formData.append("name_last", last_name);
+        formData.append("email", email);
         formData.append("password", password);
+
+        window.location.href = '/login.html';
 
         // Send a POST request to the backend to get the token
         
@@ -36,15 +43,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
             //If the response is not ok, throw an error and create an alert
 
-                  if (response.ok) {
-                    // Parse the JSON response to extract the access token
-                    const data = await response.json();
-                    
-                    // Save the access token in localStorage for future requests
-                    localStorage.setItem('token', data.access_token);
-                    
-                    // Redirect the user to the dashboard page
-                    window.location.href = 'dashboard/index.html';
+                  if (response = "success") {
+                    // redirect the user to the login page
+                    window.location.href = 'login.html';
                 } else {
                     // If login fails (e.g., 403), show the error popup
                     showErrorPopup();
@@ -68,7 +69,6 @@ document.addEventListener("DOMContentLoaded", () => {
         }, 5000);
         
     }
-
 
 
 
