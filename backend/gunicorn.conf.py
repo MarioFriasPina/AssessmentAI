f8b44046f22dbac81abfabb4a82d7ec9fd596f04
MarioFrias <mario99fpina@gmail.com>
2025-06-04 18:00:24 -0600

    Temporarily remove https

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -1,7 +1,7 @@
 """ Gunicorn configuration file for the backend server."""
 import multiprocessing
 
-bind = "0.0.0.0:443"
+bind = "0.0.0.0:80"
 workers = multiprocessing.cpu_count() * 2 + 1
 
 wsgi_app = "main:app"
